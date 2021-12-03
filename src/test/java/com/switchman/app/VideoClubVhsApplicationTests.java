bb8776be675966bb66e88c2f9db052d4164b69f6
Martinez Maximiliano <mm.martinezmaximiliano@gmail.com>
2021-12-02 21:37:37 -0300

    Primer commit creacion del proyecto

---
+++ src/test/java/com/switchman/app/VideoClubVhsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchman.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoClubVhsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
